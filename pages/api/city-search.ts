a99c231711f332cbd1ebd944c3a49d9967b93918
Bailey Stein <bailey.stein@deptagency.com>
2022-11-02 08:45:59 -0400

    Remove unused country-state-city package & respond with Response<City[]> type for city-search API endpoint

--- pages/api/city-search.ts
+++ pages/api/city-search.ts
@@ -1,14 +1,24 @@
+import dayjs from 'dayjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { CITY_SEARCH_RESULTS_MAX_AGE } from '../../constants';
 import { CitiesHelper } from '../../helpers';
 import { APIRoute, getPath, Response } from '../../models/api';
+import { City } from '../../models/cities';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const results = await CitiesHelper.searchFor(req.query);
 
+    const response: Response<City[]> = {
+      data: results,
+      warnings: [],
+      errors: [],
+      validUntil: dayjs().unix() + CITY_SEARCH_RESULTS_MAX_AGE,
+      latestReadTime: dayjs().unix()
+    };
+
     res.setHeader('Cache-Control', `public, immutable, stale-while-revalidate, max-age=${CITY_SEARCH_RESULTS_MAX_AGE}`);
-    res.status(200).json(results);
+    res.status(200).json(response);
   } catch (err) {
     console.log(`[${getPath(APIRoute.CITY_SEARCH)}]`, err);
     const errorResponse: Response<null> = {
