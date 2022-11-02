7ec276933e7646f6305d8f9d4349849db37acce4
Bailey Stein <bailey.stein@deptagency.com>
2022-11-02 12:20:04 -0400

    Re-process geonames to include non-states in the US

---
+++ .dev/process-geonames.js
@@ -0,0 +1,57 @@
+// Script to process GeoNames data into JSON format with selected relevant fields
+//  Files are sourced from: http://download.geonames.org/export/dump/
+//  Set "type": "module" in package.json, then run "node .dev/process-cities.js" in the terminal
+//  After running, "cities-formatted.json" can be formatted using "NODE_OPTIONS=--max_old_space_size=8192 npx prettier --write cities-formatted.json"
+import { readFile, writeFile } from 'fs/promises';
+
+// The following country codes are associated with the United States:
+//   US = United States
+//   AS = American Samoa
+//   GU = Guam
+//   MP = Northern Mariana Islands
+//   PR = Puerto Rico
+//   UM = United States Minor Outlying Islands
+//   VI = Virgin Islands, U.S.
+const COUNTRY_CODES = ['US', 'AS', 'GU', 'MP', 'PR', 'UM', 'VI'];
+
+// See http://www.geonames.org/export/codes.html
+const FEATURE_CODES_TO_EXCLUDE = ['', 'PPLCH', 'PPLH', 'PPLQ', 'PPLW', 'PPLX'];
+
+const run = async () => {
+  const files = await Promise.all(COUNTRY_CODES.map(code => readFile(`./.dev/${code}.txt`, 'utf-8')));
+  const cities = [];
+
+  for (let i = 0; i < files.length; ++i) {
+    const file = files[i];
+    const countryCode = COUNTRY_CODES[i];
+    const rows = file.split('\n');
+    for (const row of rows) {
+      const splitRow = row.split('\t');
+      const name = splitRow[1];
+      const featureClass = splitRow[6];
+      const featureCode = splitRow[7];
+      if (featureClass === 'P' && !FEATURE_CODES_TO_EXCLUDE.includes(featureCode) && !name.includes('(')) {
+        const alternateCityNamesIn = splitRow[3].split(',');
+        const alternateCityNames =
+          alternateCityNamesIn.length > 1 || alternateCityNamesIn[0]?.length > 0 ? alternateCityNamesIn : [];
+        cities.push({
+          cityName: splitRow[1],
+          alternateCityNames,
+          stateCode: countryCode === 'US' ? splitRow[10] : countryCode,
+          population: Number(splitRow[14]),
+          latitude: Number(splitRow[4]),
+          longitude: Number(splitRow[5]),
+          timeZone: splitRow[17],
+          geonameid: Number(splitRow[0]),
+          modified: splitRow[18]
+        });
+      }
+    }
+  }
+
+  const stringified = JSON.stringify(cities);
+  await writeFile('./public/cities.json', stringified);
+  await writeFile('./cities-formatted.json', stringified);
+};
+
+run();
