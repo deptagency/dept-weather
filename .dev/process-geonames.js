434d9ee694487de4aeb8afab53e6984aa80b4a9d
Bailey Stein <bailey.stein@deptagency.com>
2022-11-29 15:44:19 -0500

    Make geonames processing more selective & remove duplicates

--- .dev/process-geonames.js
+++ .dev/process-geonames.js
@@ -1,15 +1,19 @@
 // Script to process GeoNames data into JSON format with selected relevant fields
 //  Inputs:
-//    - "*.txt" files;
+//    - "*.txt" files, under ".dev/geoname-data";
 //        for each * in COUNTRY_CODES, sourced from: http://download.geonames.org/export/dump/
+//          The tracked *.md5 files are of the geoname files last used as input (generated with "generate-hashes.sh" script)
 //  Outputs:
 //    - "cities.json" file
 //    - "cities-formatted.json" file (unformatted output; you need to run the command below to format it)
 //  How to use:
-//    1. Run "node .dev/process-cities.js" in the terminal from the root project directory
+//    1. Run "node .dev/process-geonames.js" in the terminal from the root project directory
 //    2. Format the "cities-formatted.json" file with "NODE_OPTIONS=--max_old_space_size=8192 npx prettier --write cities-formatted.json"
 
 import { readFile, writeFile } from 'fs/promises';
+
+const cityPopulationSorter = (city1, city2) => city2.population - city1.population;
+const cityGeonameidSorter = (city1, city2) => city1.geonameid - city2.geonameid;
 
 // The following country codes are associated with the United States:
 //   US = United States
@@ -22,27 +26,30 @@ import { readFile, writeFile } from 'fs/promises';
 const COUNTRY_CODES = ['US', 'AS', 'GU', 'MP', 'PR', 'UM', 'VI'];
 
 // See http://www.geonames.org/export/codes.html
-const FEATURE_CODES_TO_EXCLUDE = ['', 'PPLCH', 'PPLH', 'PPLQ', 'PPLW', 'PPLX'];
+const FEATURE_CLASSES_AND_CODES = {
+  P: ['PPL', 'PPLA', 'PPLA2', 'PPLA3', 'PPLA4', 'PPLA5', 'PPLC']
+};
 
 const run = async () => {
-  const files = await Promise.all(COUNTRY_CODES.map(code => readFile(`./.dev/${code}.txt`, 'utf-8')));
+  const files = await Promise.all(COUNTRY_CODES.map(code => readFile(`.dev/geoname-data/${code}.txt`, 'utf-8')));
   const cities = [];
 
+  console.log('Processing cities...');
   for (let i = 0; i < files.length; ++i) {
     const file = files[i];
     const countryCode = COUNTRY_CODES[i];
     const rows = file.split('\n');
     for (const row of rows) {
       const splitRow = row.split('\t');
-      const name = splitRow[1];
+      const cityName = splitRow[1];
       const featureClass = splitRow[6];
       const featureCode = splitRow[7];
-      if (featureClass === 'P' && !FEATURE_CODES_TO_EXCLUDE.includes(featureCode) && !name.includes('(')) {
+      if (FEATURE_CLASSES_AND_CODES[featureClass]?.includes(featureCode) && !cityName.includes('(')) {
         const alternateCityNamesIn = splitRow[3].split(',');
         const alternateCityNames =
           alternateCityNamesIn.length > 1 || alternateCityNamesIn[0]?.length > 0 ? alternateCityNamesIn : [];
         cities.push({
-          cityName: splitRow[1],
+          cityName,
           alternateCityNames,
           stateCode: countryCode === 'US' ? splitRow[10] : countryCode,
           population: Number(splitRow[14]),
@@ -54,11 +61,44 @@ const run = async () => {
         });
       }
     }
+    console.log(`  ${COUNTRY_CODES[i]} processed`);
   }
 
-  const stringified = JSON.stringify(cities);
+  console.log(`Removing duplicates...`);
+  let numDupsRemoved = 0;
+  const citiesNoDuplicates = [...cities]
+    .sort(cityPopulationSorter)
+    .sort((city1, city2) => (!city1.population && !city2.population ? cityGeonameidSorter(city1, city2) : 0))
+    .map(city => ({
+      ...city,
+      cityAndStateCode: `${city.cityName}, ${city.stateCode}`
+    }));
+
+  for (let i = 0; i < citiesNoDuplicates.length; i++) {
+    for (let j = citiesNoDuplicates.length - 1; j > i; j--) {
+      if (citiesNoDuplicates[j].cityAndStateCode === citiesNoDuplicates[i].cityAndStateCode) {
+        citiesNoDuplicates.splice(j, 1);
+        if (++numDupsRemoved % 100 === 0) {
+          console.log(`  ${numDupsRemoved} duplicates removed`);
+        }
+      }
+    }
+  }
+  console.log(`  ${numDupsRemoved} total duplicates removed; size is now ${citiesNoDuplicates.length}`);
+
+  console.log('Finishing up...');
+  const cleanedCities = citiesNoDuplicates
+    .map(city => {
+      const cleanedCity = { ...city };
+      delete cleanedCity.cityAndStateCode;
+      return cleanedCity;
+    })
+    .sort(cityGeonameidSorter);
+
+  const stringified = JSON.stringify(cleanedCities);
   await writeFile('./.data/cities.json', stringified);
   await writeFile('./cities-formatted.json', stringified);
+  console.log(' OK!');
 };
 
 run();
