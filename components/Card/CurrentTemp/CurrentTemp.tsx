d045337d6f943010531df4a1e0dabb967adb079e
Bailey Stein <bailey.stein@deptagency.com>
2022-12-05 15:27:48 -0500

    Screenreader: read current temp as single item

--- components/Card/CurrentTemp/CurrentTemp.tsx
+++ components/Card/CurrentTemp/CurrentTemp.tsx
@@ -1,17 +1,49 @@
 import { WeatherObservations } from 'models/api';
+import { useEffect, useState } from 'react';
 import { floorOrEmDash, roundOrEmDash } from 'utils';
 import styles from './CurrentTemp.module.css';
 
 export default function CurrentTemp({ observations }: { observations?: WeatherObservations }) {
-  const fractionalTemp = observations?.temperature != null ? (observations.temperature * 10) % 10 : 0;
+  const [tempDisplayTxt, setTempDisplayTxt] = useState<string>('');
+  const [fractionalTemp, setFractionalTemp] = useState<string>('');
+  const [tempAriaLabel, setTempAriaLabel] = useState<string>('');
+  const [feelsLikeTxt, setFeelsLikeTxt] = useState<string>('');
+
+  useEffect(() => {
+    const newTemp = floorOrEmDash(observations?.temperature);
+    const newFractionalTemp = observations?.temperature != null ? (observations.temperature * 10) % 10 : 0;
+    let newTempAriaLabel = String(newTemp);
+    if (newFractionalTemp) {
+      newTempAriaLabel += `.${newFractionalTemp}`;
+    }
+    newTempAriaLabel += '°';
+    const newFeelsLikeTxt = `Feels Like ${roundOrEmDash(observations?.feelsLike)}°`;
+
+    setTempDisplayTxt(String(newTemp));
+    setFractionalTemp(String(newFractionalTemp));
+    setTempAriaLabel(newTempAriaLabel);
+    setFeelsLikeTxt(newFeelsLikeTxt);
+  }, [observations]);
+
   return (
     <>
-      <h3 className={styles['current-temp__temp']}>
-        <span>{floorOrEmDash(observations?.temperature)}</span>
-        {fractionalTemp ? <span className={styles['current-temp__temp__decimal']}>{`.${fractionalTemp}`}</span> : <></>}
-        <span className={fractionalTemp ? styles['current-temp__temp__degree-symbol--fractional'] : ''}>°</span>
+      <h3 className={styles['current-temp__temp']} aria-label={tempAriaLabel}>
+        <span aria-hidden={true}>{tempDisplayTxt}</span>
+        {fractionalTemp ? (
+          <span className={styles['current-temp__temp__decimal']} aria-hidden={true}>{`.${fractionalTemp}`}</span>
+        ) : (
+          <></>
+        )}
+        <span
+          className={fractionalTemp ? styles['current-temp__temp__degree-symbol--fractional'] : ''}
+          aria-hidden={true}
+        >
+          °
+        </span>
       </h3>
-      <p className={styles['current-temp__feels-like']}>Feels Like {roundOrEmDash(observations?.feelsLike)}°</p>
+      <p className={styles['current-temp__feels-like']} aria-label={feelsLikeTxt}>
+        {feelsLikeTxt}
+      </p>
     </>
   );
 }
