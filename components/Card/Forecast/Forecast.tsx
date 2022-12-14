8518d6c4025de4d40fbf00f0c597660b3a9ae010
Bailey Stein <bailey.stein@deptagency.com>
2022-12-14 17:00:25 -0500

    Use grid for aligning hourly forecast

--- components/Card/Forecast/Forecast.tsx
+++ components/Card/Forecast/Forecast.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import AnimateHeight from 'react-animate-height';
+import { HumidityIcon, PrecipitationIcon, WindIcon } from 'components/Icons';
 import { NwsHourlyPeriodForecast, NwsPeriodForecast } from 'models/api';
 import HourlyForecast from '../HourlyForecast/HourlyForecast';
 import SummaryForecast from '../SummaryForecast/SummaryForecast';
+import styles from './Forecast.module.css';
 
 const ANIMATED_CONTENTS_WRAPPER_ID = 'HourlyForecastsWrapper';
 
@@ -31,9 +33,24 @@ export default function Forecast({
         setIsExpanded={setIsExpanded}
       ></SummaryForecast>
       <AnimateHeight id={animatedContentsWrapperId} duration={300} height={isExpanded ? 'auto' : 0}>
-        {(hourlyForecasts ?? []).map((hourlyForecast, i) => (
-          <HourlyForecast key={i} forecast={hourlyForecast} isDaytime={isDaytime}></HourlyForecast>
-        ))}
+        {hourlyForecasts?.length ? (
+          <>
+            <div className={styles['forecast__hourly-forecasts']}>
+              <div></div>
+              <div></div>
+              {/* TODO - add Temperature icon */}
+              <div></div>
+              <PrecipitationIcon innerDropHeightPercent={0}></PrecipitationIcon>
+              <HumidityIcon></HumidityIcon>
+              <WindIcon directionDeg={undefined}></WindIcon>
+              {hourlyForecasts.map((hourlyForecast, i) => (
+                <HourlyForecast key={i} forecast={hourlyForecast} isDaytime={isDaytime}></HourlyForecast>
+              ))}
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </AnimateHeight>
     </>
   );
--- components/Card/HourlyForecast/HourlyForecast.tsx
+++ components/Card/HourlyForecast/HourlyForecast.tsx
@@ -1,7 +1,6 @@
 import { NwsHourlyPeriodForecast } from 'models/api';
 import { roundOrEmDash } from 'utils';
 import Condition from '../Condition/Condition';
-import styles from './HourlyForecast.module.css';
 
 export default function HourlyForecast({
   forecast,
@@ -11,13 +10,13 @@ export default function HourlyForecast({
   isDaytime: boolean;
 }) {
   return (
-    <div className={styles['hourly-forecast']}>
-      <h5 className={styles['hourly-forecast__time-label']}>{forecast.startLabel}</h5>
+    <>
+      <h5>{forecast.startLabel}</h5>
       <Condition condition={forecast.condition} size="x-small" isNight={!isDaytime}></Condition>
       <p>{`${roundOrEmDash(forecast.temperature)}°`}</p>
       <p>{`${roundOrEmDash(forecast.chanceOfPrecip)}%`}</p>
       <p>{`${roundOrEmDash(forecast.humidity)}%`}</p>
       <p>{`${roundOrEmDash(forecast.wind.speed)}mph`}</p>
-    </div>
+    </>
   );
 }
