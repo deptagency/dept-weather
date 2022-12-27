d4a042c11d4068bb07d2b1be32377b4dc72413aa
Bailey Stein <bailey.stein@deptagency.com>
2022-12-27 10:42:05 -0500

    Update meta description to reflect ability to provide alerts & hourly forecasts

--- constants/client.ts
+++ constants/client.ts
@@ -1,7 +1,7 @@
 import { SearchResultCity } from '../models/cities';
 
 export const APP_TITLE = 'DEPT® Weather';
-export const APP_DESCRIPTION = `The ${APP_TITLE} app provides up-to-date weather information and forecasts for locations across the U.S.`;
+export const APP_DESCRIPTION = `The ${APP_TITLE} app provides up-to-date weather observations, daily & hourly forecasts, and alerts for locations across the U.S.`;
 export const APP_URL = 'https://dept-weather.vercel.app';
 export const APP_THEME_COLOR = '#ffffff';
 export const APP_MASK_ICON_COLOR = '#000000';
