77afa01b7e097f716b1f39724adb860a7be41111
Bailey Stein <bailey.stein@deptagency.com>
2022-12-28 11:34:18 -0500

    Dynamic copyright year

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import homeStyles from 'styles/Home.module.css';
 import styles from './Footer.module.css';
 
 export default function Footer() {
-  return <footer className={`${styles.footer} ${homeStyles.container__content}`}>DEPT® Weather © 2022</footer>;
+  const [currentYear] = useState<number>(new Date().getUTCFullYear());
+  return <footer className={`${styles.footer} ${homeStyles.container__content}`}>DEPT® Weather © {currentYear}</footer>;
 }
