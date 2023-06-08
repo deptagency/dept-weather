d4fd4a8143c57b6ee8c0dde621339cfba1294086
Bailey Stein <bailey.stein@deptagency.com>
2023-06-08 05:31:17 -0400

    Improve air quality measurement presentation

--- components/Card/Measurement/AirQuality/AirQuality.tsx
+++ components/Card/Measurement/AirQuality/AirQuality.tsx
@@ -4,31 +4,20 @@ import { useEffect, useState } from 'react';
 import Measurement from '../Measurement';
 
 export default function AirQuality({ airnowData }: { airnowData?: AirNowObservations }) {
-  const [value, setValue] = useState<string>('–');
-  const [secondaryValue, setSecondaryValue] = useState<string>('–');
+  const [measurementInfo, setMeasurementInfo] = useState<{
+    value: string;
+    secondaryValue: string;
+    label: string;
+  }>({ value: '–', secondaryValue: '–', label: 'AQI' });
 
   useEffect(() => {
     const observation = airnowData?.observations?.length ? airnowData.observations[0] : undefined;
-
-    const newValue = String(observation?.aqi ?? '–');
-
-    let _newSecondaryValue: string | undefined = observation?.aqiLevelName ?? undefined;
-    if (observation?.pollutant) {
-      if (_newSecondaryValue != null) _newSecondaryValue += ` (${observation.pollutant})`;
-      else _newSecondaryValue = observation.pollutant;
-    }
-    const newSecondaryValue = _newSecondaryValue ?? '–';
-
-    setValue(newValue);
-    setSecondaryValue(newSecondaryValue);
+    setMeasurementInfo({
+      value: String(observation?.aqi ?? '–'),
+      secondaryValue: observation?.aqiLevelName ?? '–',
+      label: `AQI${observation?.pollutant ? ` (${observation.pollutant})` : ''}`
+    });
   }, [airnowData]);
 
-  return (
-    <Measurement
-      value={value}
-      secondaryValue={secondaryValue}
-      label="Air Quality"
-      icon={<AirQualityIcon></AirQualityIcon>}
-    />
-  );
+  return <Measurement {...measurementInfo} icon={<AirQualityIcon></AirQualityIcon>} />;
 }
