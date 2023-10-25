686dc085e072d2828a880cfc88ecbeb627e08cf0
Bailey Stein <bailey.stein@deptagency.com>
2023-10-25 17:15:24 -0400

    Revert "Use new object for return cities"
    This reverts commit b96c218ce5e571716ee6cec198b1af1aea6bc141.

--- helpers/api/cities-helper.ts
+++ helpers/api/cities-helper.ts
@@ -56,7 +56,7 @@ export class CitiesHelper {
 
   private static citiesByIdPromise: Promise<CitiesById> = (async () => {
     const citiesById = (await this.getFile(CITY_SEARCH_CITIES_BY_ID_FILENAME)) as InputCitiesById;
-    const returnCitiesById: CitiesById = {};
+    const returnCitiesById = citiesById as unknown as CitiesById;
 
     const getFormattedDuration = LoggerHelper.trackPerformance();
     for (const geonameid in citiesById) {
