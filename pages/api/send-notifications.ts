8ff8c65f241745d9fa480a320ae49409749d7b2f
Bailey Stein <bailey.stein@deptagency.com>
2023-11-06 11:30:32 -0500

    Switch to GID-only cities input for notifications

--- pages/api/send-notifications.ts
+++ pages/api/send-notifications.ts
@@ -3,8 +3,9 @@ import { API_GEONAMEID_KEY } from 'constants/shared';
 import { Alerts, NwsAlert } from 'models/api/alerts.model';
 import { APIRoute, getPath } from 'models/api/api-route.model';
 import { NotifyRequest } from 'models/api/notify.model';
+import { QueriedCityInfo } from 'models/api/queried-location-info.model';
 import { Response as ResponseModel } from 'models/api/response.model';
-import { City } from 'models/cities/cities.model';
+import { DataSource } from 'models/data-source.enum';
 import { AlertSeverity } from 'models/nws/alerts.model';
 import { PushSubscription } from 'web-push';
 
@@ -12,17 +13,12 @@ export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 interface SubscriptionInfo extends PushSubscription {
-  cities: string[];
+  note?: string;
+  cities: number[];
 }
 
 interface NotificationInfo {
   subscriptions: SubscriptionInfo[];
-  cities: City[];
-}
-
-interface AlertsForCity {
-  city: City;
-  alerts: NwsAlert[];
 }
 
 function prefixWithTime(str: string) {
@@ -30,28 +26,34 @@ function prefixWithTime(str: string) {
   return `${now.toTimeString().slice(0, 8)}.${String(now.getMilliseconds()).padStart(3, '0')} ${str}`;
 }
 
-async function getAlertsForCity(domain: string, city: City) {
-  let alerts: NwsAlert[] = [];
+async function getAlerts(domain: string, gid: number): Promise<Partial<Alerts> | undefined> {
+  let alertsResp: ResponseModel<Alerts> | undefined;
 
   try {
-    const alertsResp: ResponseModel<Alerts> = await (
-      await fetch(`${domain}${getPath(APIRoute.ALERTS, { [API_GEONAMEID_KEY]: city.geonameid })}`)
-    ).json();
-    alerts = alertsResp?.data?.nws?.alerts ?? [];
+    alertsResp = await (await fetch(`${domain}${getPath(APIRoute.ALERTS, { [API_GEONAMEID_KEY]: gid })}`)).json();
   } catch (err) {
-    console.error(`Could not get alerts for ${city.geonameid} / ${city.cityName}, ${city.stateCode}`, err);
+    console.error(`Could not get alerts for ${gid}`, err);
+    if (alertsResp?.errors?.length) console.error(`Returned errors:`, alertsResp.errors);
+    if (alertsResp?.errors?.length) console.error(`Returned warnings:`, alertsResp.warnings);
   }
 
-  return { alerts, city };
+  return alertsResp?.data;
 }
 
-async function notify(domain: string, subscription: SubscriptionInfo, city: City, alert: NwsAlert, authHeader: string) {
+async function notify(
+  domain: string,
+  subscription: SubscriptionInfo,
+  alert: NwsAlert,
+  city: QueriedCityInfo[DataSource.QUERIED_CITY] | undefined,
+  authHeader: string
+) {
+  const toStr = subscription.note ?? subscription.endpoint.slice(-6);
   let notifyResp: Response | undefined;
   try {
     const severityFName = alert.severity !== 'Unknown' ? alert.severity : 'Minor';
     const notifyRequest: NotifyRequest = {
       subscription,
-      title: `${alert.title} – ${city.cityName}, ${city.stateCode}`,
+      title: city != null ? `${alert.title} – ${city.cityName}, ${city.stateCode}` : alert.title,
       notificationOptions: {
         tag: alert.id,
         body: `${
@@ -76,54 +78,53 @@ async function notify(domain: string, subscription: SubscriptionInfo, city: City
       body: JSON.stringify(notifyRequest)
     });
     if (notifyResp.ok) {
-      return `Sent notification for alert.id: ${alert.id.slice(-13)} to ${subscription.endpoint.slice(-6)} – ${
-        notifyResp.status
-      }`;
+      return `Sent notification for alert.id: ${alert.id.slice(-13)} to ${toStr} – ${notifyResp.status}`;
     }
   } catch {
     /* empty */
   }
 
   console.error(
-    `Could not send notification for alert.id: ${alert.id.slice(-13)} to ${subscription.endpoint.slice(
-      -6
-    )}  – ${notifyResp?.status}: ${(await notifyResp?.text())?.trim()}`
+    `Could not send notification for alert.id: ${alert.id.slice(-13)} to ${toStr}  – ${notifyResp?.status}: ${(
+      await notifyResp?.text()
+    )?.trim()}`
   );
 }
 
-async function* notifications(domain: string, { cities, subscriptions }: NotificationInfo, authHeader: string) {
-  const alertsForCityMap = new Map<string, Promise<[string, AlertsForCity]>>(
-    cities.map(city => [city.geonameid, getAlertsForCity(domain, city).then(res => [city.geonameid, res])])
+async function* notifications(domain: string, { subscriptions }: NotificationInfo, authHeader: string) {
+  const subscriptionsForGids = new Map<number, number[]>();
+  for (let i = 0; i < subscriptions.length; i++) {
+    for (const gid of subscriptions[i].cities) {
+      const prevVal = subscriptionsForGids.get(gid) ?? [];
+      subscriptionsForGids.set(gid, [...prevVal, i]);
+    }
+  }
+  console.log('subscriptionsForGids:', subscriptionsForGids);
+  const alertsForCityMap = new Map<number, Promise<[number, Partial<Alerts> | undefined]>>(
+    Array.from(subscriptionsForGids.keys()).map(gid => [gid, getAlerts(domain, gid).then(res => [gid, res])])
   );
   while (alertsForCityMap.size) {
-    const [key, result] = await Promise.race(alertsForCityMap.values());
+    const [gid, result] = await Promise.race(alertsForCityMap.values());
 
-    if (result.alerts.length) {
-      const subscribedSubs = subscriptions.filter(subscription => subscription.cities.includes(result.city.geonameid));
-      console.info(
-        `${result.alerts.length} alerts & ${subscribedSubs.length} subscriptions for "${result.city.cityName}, ${result.city.stateCode}" / ${result.city.geonameid}`
-      );
-      if (subscribedSubs.length) {
-        for (const alert of result.alerts) {
-          const notifyMap = new Map<string, Promise<[string, string | undefined]>>(
-            subscribedSubs.map(subscription => [
-              subscription.endpoint.slice(-6),
-              notify(domain, subscription, result.city, alert, authHeader).then(res => [
-                subscription.endpoint.slice(-6),
-                res
-              ])
-            ])
-          );
-          while (notifyMap.size) {
-            const [key, result] = await Promise.race(notifyMap.values());
-            if (result != null) yield prefixWithTime(result);
-            notifyMap.delete(key);
-          }
+    if (result?.nws?.alerts?.length) {
+      const subIdxs = subscriptionsForGids.get(gid)!;
+      console.info(`${result.nws.alerts.length} alerts & ${subIdxs.length} subscriptions for ${gid}`);
+      for (const alert of result.nws.alerts) {
+        const notifyMap = new Map<number, Promise<[number, string | undefined]>>(
+          subIdxs.map(idx => [
+            idx,
+            notify(domain, subscriptions[idx], alert, result.queriedCity, authHeader).then(res => [idx, res])
+          ])
+        );
+        while (notifyMap.size) {
+          const [idx, result] = await Promise.race(notifyMap.values());
+          if (result != null) yield prefixWithTime(result);
+          notifyMap.delete(idx);
         }
       }
     }
 
-    alertsForCityMap.delete(key);
+    alertsForCityMap.delete(gid);
   }
 
   yield prefixWithTime('Finished!');
@@ -141,9 +142,7 @@ export default async function GET(req: NextRequest) {
   const notificationInfo = (await (
     await fetch(`${process.env.NOTIFICATIONS_INFO_URL!}?cache-bust=${new Date().getTime() / 1_000}`)
   ).json()) as NotificationInfo;
-  console.info(
-    `Retrieved info for ${notificationInfo.subscriptions.length} subscriptions, ${notificationInfo.cities.length} cities`
-  );
+  console.info(`Retrieved info for ${notificationInfo.subscriptions.length} subscriptions`);
 
   const domain = req.url.slice(0, req.url.indexOf(getPath(APIRoute.SEND_NOTIFICATIONS)));
   const iterator = notifications(domain, notificationInfo, authHeader);
